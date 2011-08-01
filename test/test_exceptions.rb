49333403d97ce5a1dc98abfc2b233aa42e884e4c
Matthew Savage <matt@amasses.net>
2011-08-02 07:34:02 +1000

    Removed external dependencies for unit tests

---
+++ test/test_exceptions.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestExceptions < Test::Unit::TestCase
+  def setup
+    @connection = Weatherzone::Connection.new("username", "password", nil, :timeout_after => 10)
+  end
+  
+  def test_request_failed_should_capture_original_exception
+    original = Timeout::Error.new
+    exception = Weatherzone::RequestFailed.new("url", original)
+    assert_instance_of Timeout::Error, exception.original_exception
+  end
+    
+  def test_connection_should_raise_request_failed_on_timeout
+    mock_uri = mock()
+    mock_uri.expects(:read).raises(Timeout::Error)
+    URI.stubs(:parse).returns(mock_uri)
+    
+    @connection.timeout_after = 0.1
+    assert_raises Weatherzone::RequestFailed do
+      Weather.find_by_location_name(@connection, "Sydney")
+    end
+  end
+    
+end
